61f3c23db4c018a0a6d0c72367b09dd7c80a814f
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-03-18 16:26:24 +0100

    add demo data

---
+++ data/projects.ts
@@ -0,0 +1,36 @@
+import Project from "@/models/project";
+
+const projects: Project[] = [
+  {
+    id: 1,
+    name: "Project 1",
+    description: "This is project 1",
+    imageUrl: "https://picsum.photos/200",
+    githubUrl: "https://github.com",
+    demoUrl: "https://demo.com",
+  },
+  {
+    id: 2,
+    name: "Project 2",
+    description: "This is project 2",
+    imageUrl: "https://picsum.photos/200",
+    githubUrl: "https://github.com",
+  },
+  {
+    id: 3,
+    name: "Project 3",
+    description: "This is project 3",
+    imageUrl: "https://picsum.photos/200",
+    githubUrl: "https://github.com",
+    demoUrl: "https://demo.com",
+  },
+  {
+    id: 4,
+    name: "Project 4",
+    description: "This is project 4",
+    imageUrl: "https://picsum.photos/200",
+    githubUrl: "https://github.com",
+  }
+];
+
+export default projects;
