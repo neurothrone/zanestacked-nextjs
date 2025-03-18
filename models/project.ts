89415f96ec9d5416e113d9db95b95b8420847a78
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-03-18 16:26:02 +0100

    add Project and Skill interfaces

---
+++ models/project.ts
@@ -0,0 +1,11 @@
+interface Project {
+  // id: string;
+  id: number;
+  name: string;
+  description: string;
+  imageUrl: string;
+  githubUrl: string;
+  demoUrl?: string;
+}
+
+export default Project;
---
+++ models/skill.ts
@@ -0,0 +1,10 @@
+interface Skill {
+  // id: string;
+  id: number;
+  name: string;
+  yearsOfExperience: number;
+  proficiency: string;
+  createdDate: string;
+}
+
+export default Skill;
