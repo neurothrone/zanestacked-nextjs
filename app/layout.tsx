69a8a0badabee4e41ff374c7511edd79d7defbdc
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-03-20 17:14:26 +0100

    update layout to use variable font for improved styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const orbitron = Orbitron({
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-    <body className={`${orbitron.className} antialiased`}>
+    <body className={`${orbitron.variable} ${orbitron.className} antialiased`}>
       {children}
     </body>
     </html>
