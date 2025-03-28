5211827dce415a31322833ccbc485d6141d38324
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-03-28 12:27:35 +0100

    feat: add buttons for creating, editing, and deleting projects and skills

---
+++ src/ui/components/admin/skills/buttons.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { deleteSkill } from "@/src/lib/actions";
+
+const CreateSkill = () => {
+  return (
+    <Link
+      href="/admin/skills/add"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+    >
+      <span className="hidden md:block">Create Skill</span>{" "}
+      <PlusIcon className="h-5 md:ml-4"/>
+    </Link>
+  );
+}
+
+const EditSkill = ({ id }: { id: string }) => {
+  return (
+    <Link
+      href={`/admin/skills/edit/${id}`}
+      className="rounded-md border p-2 hover:bg-gray-100"
+    >
+      <PencilIcon className="w-5"/>
+    </Link>
+  );
+}
+
+const DeleteSkill = ({ id }: { id: string }) => {
+  const deleteSkillWithId = deleteSkill.bind(null, id);
+
+  return (
+    <form action={deleteSkillWithId}>
+      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+        <span className="sr-only">Delete</span>
+        <TrashIcon className="w-5"/>
+      </button>
+    </form>
+  );
+}
+
+export { CreateSkill, EditSkill, DeleteSkill };
