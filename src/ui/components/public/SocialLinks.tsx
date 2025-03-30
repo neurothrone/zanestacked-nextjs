1182cd1136d3b769288d249abcd387bc7c58eafc
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-03-30 21:03:50 +0200

    feat: adjust padding in SocialLinks component for improved layout

--- src/ui/components/public/SocialLinks.tsx
+++ src/ui/components/public/SocialLinks.tsx
@@ -3,7 +3,7 @@ import PrimaryButton from "@/src/ui/components/public/PrimaryButton";
 
 const SocialLinks = () => {
   return (
-    <div className="flex justify-center gap-4 md:gap-6 py-8 border-y border-violet-900">
+    <div className="flex justify-center gap-4 py-6 border-y border-violet-900">
       <PrimaryButton href="https://github.com/neurothrone">
         <FaGithub className="inline mr-2 -mt-0.5"/>
         <span className="text-base">GitHub</span>
